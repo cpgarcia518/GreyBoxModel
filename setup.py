92c50ed747ea2136f63561e3b29f97a9a2719576
MSc. Carlos Alejandro Perez Garcia <cpgarcia518@gamil.com>
2023-10-02 17:11:19 +0300

    Installing the package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    packages=find_packages(),
+    version="1.0.0",
+    description="A Python package for Grey Box Modelling",
+    author="Carlos Alejandro Perez Garcia",
+    author_email='cpgarcia518@gmail.com',
+    url='https://github.com/cpgarcia518/GreyBoxModel',
+    license="MIT",
+)
--- src/greyboxmodel/__init__.py
+++ src/greyboxmodel/__init__.py
@@ -2,7 +2,17 @@
 #   Copyright (c) Microsoft Corporation. All rights reserved.
 #   Licensed under the MIT License. See LICENSE in project root for information.
 #   -------------------------------------------------------------
-"""Python Package Template"""
+"""
+Main Settings
+"""
 from __future__ import annotations
 
-__version__ = "0.0.2"
+__author__ = "Carlos Alejandro Perez Garcia"
+__copyright__ = "Copyright 2023"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Carlos Alejandro Perez Garcia"
+__email__ = "cpgarcia518@gmail.com"
+
+
+
