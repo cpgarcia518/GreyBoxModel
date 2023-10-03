5b20b63582cafa0cc445468e39e5e949c2fb25b4
MSc. Carlos Alejandro Perez Garcia <cpgarcia518@gamil.com>
2023-10-03 14:48:05 +0300

    Adding Ti Model

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import find_packages, setup
 
 setup(
     name="models",
-    packages=find_packages(),
-    py_modules=['greyboxmodel'],
+    # packages=find_packages(),
+    # packages=['greyboxmodel'],
+    package_dir={'greyboxmodel': 'src/greyboxmodel'},
     version="1.0.0",
     description="A Python package for Grey Box Modelling",
     author="Carlos Alejandro Perez Garcia",
